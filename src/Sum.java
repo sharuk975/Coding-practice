14bfa2b4f1c565a02fe8670a258b11dcf1262395
sharuk <sharukshaik975@gmail.com>
2025-11-30 23:52:39 +0530

    Sum/java

--- src/Sum.java
+++ src/Sum.java
@@ -2,6 +2,6 @@ public class Sum {
     public static void main(String[] args) {
         int a = 98;
         int b = 99;
-        System.out.println("Sum is: " + (a+b));
+        System.out.println("Sum is: " + (a+b)); //adding two variables
     }
 }
