14d37500bfd4a610542e661ca08f660159f26681
sharuk <sharukshaik975@gmail.com>
2025-12-05 23:45:33 +0530

    LargeNO/java

---
+++ src/LargeNo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LargeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        if(x > y && z < x) {
+            System.out.println("The is the largest number" + x);
+        } else if(y > x && z < y) {
+            System.out.println("The is the largest number" + y);
+        } else {
+            System.out.println("The is the largest number " + z);
+        }
+    }
+}
