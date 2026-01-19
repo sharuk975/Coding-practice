42604eeba63c5f5fd3f6e39a146fe6e8f7b76a9b
sharuk <sharukshaik975@gmail.com>
2026-01-19 23:32:32 +0530

    PrintNumbers/java

---
+++ src/PrintNumbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class PrintNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a no:");
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
